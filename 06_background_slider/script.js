025537bd71b2da294d07533ffd114924e8c52198
lance from huaweiLaptop <gggreenlance@gmail.com>
2023-03-24 15:58:57 +0800

    03-24 2nd practice 06-background_slider

--- 06_background_slider/script.js
+++ 06_background_slider/script.js
@@ -1,40 +1,32 @@
-const body = document.body;
-const slides = document.querySelectorAll(".slide");
-const leftBtn = document.getElementById("left");
-const rightBtn = document.getElementById("right");
+const silders = document.querySelectorAll(".slide");
+const left_arrow = document.querySelector("#left-arrow");
+const right_arrow = document.querySelector("#right-arrow");
 
-let activeSlide = 0;
+document.body.style.backgroundImage =
+  "url('https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')";
 
-rightBtn.addEventListener("click", () => {
-  activeSlide++;
+let imgIndex = 0;
 
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0;
-  }
-
-  setBgToBody();
-  setActiveSlide();
+left_arrow.addEventListener("click", () => {
+  --imgIndex;
+  changeBackgroundImg();
+});
+right_arrow.addEventListener("click", () => {
+  ++imgIndex;
+  changeBackgroundImg();
 });
 
-leftBtn.addEventListener("click", () => {
-  activeSlide--;
-
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1;
+function changeBackgroundImg() {
+  if (imgIndex <= -1) {
+    imgIndex = 4;
+  } else if (imgIndex >= 5) {
+    imgIndex = 0;
   }
-
-  setBgToBody();
-  setActiveSlide();
-});
-
-setBgToBody();
-
-function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
-}
-
-function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove("active"));
-
-  slides[activeSlide].classList.add("active");
+  console.log(silders);
+  silders.forEach((silder) => {
+    silder.classList.remove("active");
+  });
+  silders[imgIndex].classList.add("active");
+  const imgUrl = silders[imgIndex].style.backgroundImage;
+  document.body.style.backgroundImage = imgUrl;
 }
