72521f8e8569c980b927c616962f6bf99961d740
lance from huaweiLaptop <gggreenlance@gmail.com>
2023-03-27 13:17:46 +0800

    03-27 2nd practice 03project

--- 03_animated--countdown/script.js
+++ 03_animated--countdown/script.js
@@ -35,6 +35,8 @@ function finalShow() {
   finalMessage.classList.add("show");
   counter.classList.add("hide");
 }
+
 replay.addEventListener("click", () => {
   resetDOM();
+  runAnimation();
 });
